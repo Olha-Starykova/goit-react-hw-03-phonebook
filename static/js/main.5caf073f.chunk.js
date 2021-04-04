a4d0477d648bc492be247ddff2845946686270f7
Olha-Starykova <olenkalog@gmail.com>
2021-04-05 01:44:40 +0300

    Updates

---
+++ static/js/main.5caf073f.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-03-phonebook"]=this["webpackJsonpgoit-react-hw-03-phonebook"]||[]).push([[0],{9:function(e,t,o){"use strict";o.r(t);var n=o(1),c=o.n(n),r=o(3),i=o.n(r),s=o(0);var a=function(){return Object(s.jsx)("div",{children:"olga"})};i.a.render(Object(s.jsx)(c.a.StrictMode,{children:Object(s.jsx)(a,{})}),document.getElementById("root"))}},[[9,1,2]]]);
+//# sourceMappingURL=main.5caf073f.chunk.js.map
